e12f9fbe954269eecab49126bdd616c5d0d56436
Eveliis <eveliis.kallas@khk.ee>
2019-05-14 12:35:08 +0300

    Turned Setting Activity to First Activity. Added information to spinners and added click event.

--- app/src/main/java/com/example/tarviliivak/expandablelistview/Setting_list.java
+++ app/src/main/java/com/example/tarviliivak/expandablelistview/Setting_list.java
@@ -1,5 +1,7 @@
 package com.example.tarviliivak.expandablelistview;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -7,26 +9,89 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.Toast;
+
+import java.sql.Array;
 
 public class Setting_list extends Fragment {
+
+    ArrayAdapter adapter;
+    Spinner spinner;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final Resources res = getResources();
+
 
         View view = inflater.inflate(R.layout.fragment_setting_list, container, false);
-        Button btnFragment=(Button)view.findViewById(R.id.back);
+        Button btnFragment = (Button) view.findViewById(R.id.back);
 
-        btnFragment.setOnClickListener(new View.OnClickListener(){
+        btnFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction fr =getFragmentManager().beginTransaction();
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
                 fr.replace(R.id.container, new Setting_btn());
                 fr.commit();
             }
         });
+
+        spinner = (Spinner) view.findViewById(R.id.group_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getApplicationContext(),
+                R.array.group_arrays, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                    Intent myIntent = new Intent(view.getContext(), Auto.class);
+                    startActivityForResult(myIntent, 0);
+                }
+                if(position==1){
+                    Intent myIntent = new Intent(view.getContext(), Business.class);
+                    startActivityForResult(myIntent, 1);
+                }
+                if(position==2){
+                    Intent myIntent = new Intent(view.getContext(), Construction.class);
+                    startActivityForResult(myIntent, 2);
+                }
+                if(position==3){
+                    Intent myIntent = new Intent(view.getContext(), ICT.class);
+                    startActivityForResult(myIntent, 3);
+                }
+                if(position==4){
+                    Intent myIntent = new Intent(view.getContext(), Beauty_textile.class);
+                    startActivityForResult(myIntent, 4);
+                }
+                if(position==5){
+                    Intent myIntent = new Intent(view.getContext(), Tech.class);
+                    startActivityForResult(myIntent, 5);
+                }
+                if(position==6){
+                    Intent myIntent = new Intent(view.getContext(), Food_tourism.class);
+                    startActivityForResult(myIntent, 6);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         return view;
 
     }
